ff9956419067fa2ecc4e6f5fab81fbbfed284ae3
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-01-23 17:24:38 +0200

    Removed custom exception handler

--- app/graphql/GraphQL.scala
+++ app/graphql/GraphQL.scala
@@ -2,11 +2,7 @@ package graphql
 
 import com.google.inject.{Inject, Singleton}
 import graphql.schema.PostSchema
-import models.errors.{InvalidTitle, TooComplexQueryError}
-import sangria.execution.{ExceptionHandler, HandledException, MaxQueryDepthReachedError}
 import sangria.schema.{ObjectType, fields}
-import sangria.validation.UndefinedFieldViolation
-import sangria.marshalling.MarshallingUtil._
 
 /**
   * Defines the global GraphQL-related objects of the application.
@@ -40,33 +36,5 @@ class GraphQL @Inject()(val postSchema: PostSchema) {
         )
       )
     )
-  )
-
-  /**
-    * The exception handler that defines custom error handling mechanism.
-    */
-  val exceptionHandler = ExceptionHandler(
-    onException = {
-      case (resultMarshaller, error: InvalidTitle) => HandledException(
-        error.getMessage,
-        Map(
-          "validation_rule" -> resultMarshaller.fromString("The following symbols are allowed: [a-z,A-Z,0-9,- ]. From 3 to 100 symbols.")
-        ),
-        addFieldsInError = true,
-        addFieldsInExtensions = false
-      )
-      case (_, error: TooComplexQueryError) => HandledException(error.getMessage)
-      case (_, error: MaxQueryDepthReachedError) => HandledException(error.getMessage)
-    },
-    onViolation = {
-      case (resultMarshaller, violation: UndefinedFieldViolation) =>
-        HandledException(
-          "Field is missing!",
-          Map(
-            "fieldName" -> resultMarshaller.fromString(violation.fieldName),
-            "errorCode" -> resultMarshaller.fromString("FIELD_MISSING")
-          )
-        )
-    }
   )
 }
