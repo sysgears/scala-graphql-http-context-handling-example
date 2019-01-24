19e38c116f71f1ae4f2d5f13cdc5a5505bc0ed88
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-01-24 15:25:53 +0200

    Renamed 'HttpContext' -> 'Context'

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import com.google.inject.{Inject, Singleton}
-import graphql.{GraphQL, HttpContext}
+import graphql.{GraphQL, Context}
 import models.errors.{TooComplexQueryError, UnsupportedBodyTypeError}
 import play.api.libs.json._
 import play.api.mvc._
@@ -62,7 +62,7 @@ class AppController @Inject()(controllerComponents: ControllerComponents,
 
       maybeQuery match {
         case Success((query, operationName, variables)) =>
-          val httpContext = HttpContext(request.headers)
+          val httpContext = Context(request.headers)
           executeQuery(query, variables, operationName, httpContext)
             .map(_.withHeaders(httpContext.newHeaders: _*).withCookies(httpContext.newCookies: _*))
         case Failure(error) => Future.successful {
@@ -101,15 +101,15 @@ class AppController @Inject()(controllerComponents: ControllerComponents,
   def executeQuery(query: String,
                    variables: Option[JsObject] = None,
                    operation: Option[String] = None,
-                   context: HttpContext): Future[Result] = QueryParser.parse(query) match {
+                   context: Context): Future[Result] = QueryParser.parse(query) match {
     case Success(queryAst: Document) => Executor.execute(
       schema = graphQL.Schema,
       queryAst = queryAst,
       userContext = context,
       variables = variables.getOrElse(Json.obj()),
       queryReducers = List(
-        QueryReducer.rejectMaxDepth[HttpContext](graphQL.maxQueryDepth),
-        QueryReducer.rejectComplexQueries[HttpContext](graphQL.maxQueryComplexity, (_, _) => TooComplexQueryError())
+        QueryReducer.rejectMaxDepth[Context](graphQL.maxQueryDepth),
+        QueryReducer.rejectComplexQueries[Context](graphQL.maxQueryComplexity, (_, _) => TooComplexQueryError())
       )
     ).map(Ok(_)).recover {
       case error: QueryAnalysisError ⇒ BadRequest(error.resolveError)
