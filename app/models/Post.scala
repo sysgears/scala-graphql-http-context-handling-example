09fe945a8df5f41d1605fe39a2024ab690fdf9d5
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-01-24 15:40:44 +0200

    Added authorId field to the Post class

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -3,10 +3,12 @@ package models
 /**
   * The Post entity.
   *
-  * @param id      an id of the post
-  * @param title   a title of the post
-  * @param content a content of the post
+  * @param id       an id of the post
+  * @param authorId an id of the post's author
+  * @param title    a title of the post
+  * @param content  a content of the post
   */
 case class Post(id: Option[Long] = None,
+                authorId: String,
                 title: String,
                 content: String)
